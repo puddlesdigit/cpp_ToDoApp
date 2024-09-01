f74ec00c04ab8e6a79f5cd65760d8e449a73a853
MRPodle <vreg2391@outlook.com>
2024-09-01 12:13:40 -0400

    removed pointer from class

--- main.cpp
+++ main.cpp
@@ -15,25 +15,16 @@ public:
         return value;
     }
 
-    int getValuePtr() const {
-        return *ptr;
-    }
-
 private:
     int value;
-    int* ptr = &value;
 };
 
 int main() {
     MyClass obj(10);
     std::cout << "Initial Value: " << obj.getValue() << std::endl;
 
-    std::cout << "Initial Value (ptr): " << obj.getValuePtr() << std::endl;
-
     obj.setValue(20);
     std::cout << "New Value: " << obj.getValue() << std::endl;
-
-    std::cout << "New Value (ptr): " << obj.getValuePtr() << std::endl;
 
     return 0;
 }
